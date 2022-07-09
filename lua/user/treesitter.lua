d2630a738c3d06bf6804ff1c96dda73e63d40537
yuyu <e24076459@gs.ncku.edu.tw>
2022-07-09 21:07:10 +0800

    Apply plugin settings from old config

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -13,5 +13,5 @@ configs.setup({
 	autopairs = {
 		enable = true,
 	},
-	indent = { enable = true, disable = { "python", "css" } },
+	indent = { enable = true, disable = { "cpp", "c", "python", "css" } },
 })
