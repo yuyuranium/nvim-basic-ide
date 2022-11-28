74fe9658cfa19b37c25fef5140a0575e99a404fe
yuyuranium <n28111534@gs.ncku.edu.tw>
2022-11-28 21:39:42 +0800

    Disable rnu for now

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -89,15 +89,15 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
-
-vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave" }, {
-	callback = function()
-		vim.cmd("set rnu")
-	end,
-})
-
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter" }, {
-	callback = function()
-		vim.cmd("set nornu")
-	end,
-})
+--
+-- vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave" }, {
+-- 	callback = function()
+-- 		vim.cmd("set rnu")
+-- 	end,
+-- })
+--
+-- vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter" }, {
+-- 	callback = function()
+-- 		vim.cmd("set nornu")
+-- 	end,
+-- })
