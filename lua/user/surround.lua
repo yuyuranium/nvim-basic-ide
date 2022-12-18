3996a6116bd6e28c1d118f63b122c55177e12f89
yuyuranium <n28111534@gs.ncku.edu.tw>
2022-12-18 10:53:26 +0800

    Use  as visual surround keymap

--- lua/user/surround.lua
+++ lua/user/surround.lua
@@ -1,3 +1,20 @@
-require("nvim-surround").setup({
+local status_ok, surround = pcall(require, "nvim-surround")
+if not status_ok then
+	return
+end
+
+surround.setup({
   -- Configuration here, or leave empty to use defaults
+  keymaps = {
+    insert = "<C-g>s",
+    insert_line = "<C-g>S",
+    normal = "ys",
+    normal_cur = "yss",
+    normal_line = "yS",
+    normal_cur_line = "ySS",
+    visual = "s",
+    visual_line = "gs",
+    delete = "ds",
+    change = "cs",
+  },
 })
