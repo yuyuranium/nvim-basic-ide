4ada18d997fc6f3fac73cca380e74080ae5f9e35
yuyuranium <n28111534@gs.ncku.edu.tw>
2023-04-19 14:16:10 +0800

    Update toggleterm

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -37,13 +37,6 @@ function M.config()
   end
 
   vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
-
-  local Terminal = require("toggleterm.terminal").Terminal
-  local lazygit = Terminal:new { cmd = "lazygit", hidden = true }
-
-  function _LAZYGIT_TOGGLE()
-    lazygit:toggle()
-  end
 end
 
 return M
