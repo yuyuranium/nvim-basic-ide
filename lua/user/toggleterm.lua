80396f1322694064d9d3fb7136c8758b4e32942e
yuyuranium <n28111534@gs.ncku.edu.tw>
2023-04-19 10:45:34 +0800

    No toggle term

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -31,10 +31,10 @@ end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
-local Terminal = require("toggleterm.terminal").Terminal
+-- local Terminal = require("toggleterm.terminal").Terminal
 -- local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })  -- I don't use lazygit
-
-function _LAZYGIT_TOGGLE()
-	lazygit:toggle()
-end
+--
+-- function _LAZYGIT_TOGGLE()
+-- 	lazygit:toggle()
+-- end
 
