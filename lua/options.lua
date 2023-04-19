2ba56560d3964d65a675d566d30e2495d8fffebb
yuyuranium <n28111534@gs.ncku.edu.tw>
2023-04-19 11:49:44 +0800

    Update options

--- lua/options.lua
+++ lua/options.lua
@@ -32,12 +32,12 @@ vim.opt.ruler = false                           -- hide the line and column numb
 vim.opt.numberwidth = 4                         -- minimal number of columns to use for the line number {default 4}
 vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
 vim.opt.wrap = false                            -- display lines as one long line
-vim.opt.scrolloff = 8                           -- minimal number of screen lines to keep above and below the cursor
+vim.opt.scrolloff = 3                           -- minimal number of screen lines to keep above and below the cursor
 vim.opt.sidescrolloff = 8                       -- minimal number of screen columns to keep to the left and right of the cursor if wrap is `false`
 vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
 vim.opt.fillchars.eob = " "                     -- show empty lines at the end of a buffer as ` ` {default `~`}
 vim.opt.shortmess:append "c"                    -- hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
-vim.opt.whichwrap:append "<,>,[,],h,l"          -- keys allowed to move to the previous/next line when the beginning/end of line is reached
-vim.opt.iskeyword:append "-"                    -- treats words with `-` as single words
-vim.opt.formatoptions:remove { "c", "r", "o" }  -- This is a sequence of letters which describes how automatic formatting is to be done
+vim.opt.whichwrap:append "<,>,[,]"              -- keys allowed to move to the previous/next line when the beginning/end of line is reached
+vim.opt.formatoptions:remove { "r", "o" }       -- This is a sequence of letters which describes how automatic formatting is to be done
+vim.opt.textwidth = 100
 vim.opt.linebreak = true
