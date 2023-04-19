d0e3f7db2e5d4cd372750c154eb3a5fb1a9a4d6d
yuyuranium <n28111534@gs.ncku.edu.tw>
2023-04-19 14:10:21 +0800

    Update null-ls

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -26,10 +26,8 @@ function M.config()
         extra_filetypes = { "toml" },
         extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
       },
-      formatting.black.with { extra_args = { "--fast" } },
       formatting.stylua,
       formatting.google_java_format,
-      diagnostics.flake8,
     },
   }
 end
