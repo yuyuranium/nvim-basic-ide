a9600b36bb135e99d2b58e6c8eed4e41d1d7b9f1
yuyuranium <n28111534@gs.ncku.edu.tw>
2023-07-21 14:15:03 +0800

    Don't use Makefile to detect project

--- lua/user/project.lua
+++ lua/user/project.lua
@@ -19,7 +19,7 @@ function M.config()
     detection_methods = { "pattern" },
 
     -- patterns used to detect root dir, when **"pattern"** is in detection_methods
-    patterns = { ".git", "Makefile", "package.json" },
+    patterns = { ".git", "package.json" },
   }
 
   local telescope = require "telescope"
